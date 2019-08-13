01c15fdf3f6a6df48c7336998d47686619112358
vnutir-snega <snezha.goldshteyn@gmail.com>
2019-08-13 21:34:35 +0300

    Name refactoring

--- src/main/java/random/randomvalues/annotation/RandomValue.java
+++ src/main/java/random/randomvalues/annotation/RandomValue.java
@@ -7,6 +7,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-public @interface GetRandomValue {
+public @interface RandomValue {
     Types type();
 }
--- src/main/java/random/randomvalues/annotation/RandomValueAnnotationBeanPostProcessor.java
+++ src/main/java/random/randomvalues/annotation/RandomValueAnnotationBeanPostProcessor.java
@@ -2,28 +2,26 @@ package random.randomvalues.annotation;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ReflectionUtils;
 import random.randomvalues.handlers.RandomHandler;
 
 import java.lang.reflect.Field;
-import java.util.List;
 import java.util.Map;
 
 import static java.util.stream.Collectors.toMap;
 
 @Component
 @RequiredArgsConstructor
-public class GetRandomValueAnnotationBeanPostProcessor implements BeanPostProcessor {
+public class RandomValueAnnotationBeanPostProcessor implements BeanPostProcessor {
     private final Map <Types, RandomHandler> map;
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         Field [] fields = bean.getClass().getDeclaredFields();
         for (Field field : fields) {
-            GetRandomValue randomValue = field.getAnnotation(GetRandomValue.class);
+            RandomValue randomValue = field.getAnnotation(RandomValue.class);
             if (randomValue != null) {
                 Types type = randomValue.type();
                 RandomHandler handler = map.get(type);
